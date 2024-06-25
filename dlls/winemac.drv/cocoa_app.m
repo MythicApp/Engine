30beb7d6a956d6c6d1885412cc5d4697bdfb30a7
Josh <36625023+JoshuaBrest@users.noreply.github.com>
2024-06-24 22:18:30 -0400

    Formatting

--- dlls/winemac.drv/cocoa_app.m
+++ dlls/winemac.drv/cocoa_app.m
@@ -905,6 +905,7 @@ static NSString* WineLocalizedString(unsigned int stringID)
                 NSLog(@"setProcessName: Failed to get _LSSetApplicationInformationItem in LaunchServices");
                 return false;
             }
+
             // Get the display name key
             const CFStringRef* displayNameKey = (const CFStringRef*)(
                 CFBundleGetDataPointerForName(launchServicesBundle, CFSTR("_kLSDisplayNameKey"))
@@ -914,6 +915,7 @@ static NSString* WineLocalizedString(unsigned int stringID)
                 NSLog(@"setProcessName: Failed to get _kLSDisplayNameKey in LaunchServices");
                 return false;
             }
+
             // Force symbols to be loaded in the LaunchServices framework
             ColorSyncCMMGetTypeID();
         }
